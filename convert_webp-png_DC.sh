59f9f2e5a7756cb787040ef3a513ef8720f2d044
B P <aslermelean@gmail.com>
2025-05-25 11:24:37 +0300

    3

--- convert_webp-png_DC.sh
+++ convert_webp-png_DC.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
+SOFT='webp'
+
+if ! command -v "$SOFT" >/dev/null 2>&1 ; then
+	echo Install "$SOFT"
+	sudo apt install webp
+fi   
+
 #имя без расширения
 dwebp "$1" -o "${1%.*}".png && rm "$1"
