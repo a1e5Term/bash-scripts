aadc09233402abf7e389a17e786b5dbd1a6d9aba
B P <aslermelean@gmail.com>
2025-05-25 18:48:12 +0300

    ветка1-1

--- convert_webp-png_DC.sh
+++ convert_webp-png_DC.sh
@@ -1,10 +1,35 @@
 #! /bin/bash
+
 SOFT='webp'
 
-if ! command -v "$SOFT" >/dev/null 2>&1 ; then
+if [ ! command -v "$SOFT" >/dev/null 2>&1 ] ; then
 	echo Install "$SOFT"
 	sudo apt install webp
+	if [ ! command -v "$SOFT" >/dev/null 2>&1 ] ; then
+		exit 0
+	fi
 fi   
 
-#имя без расширения
-dwebp "$1" -o "${1%.*}".png && rm "$1"
+convrt(){
+	#имя без расширения
+	dwebp "$1" -o "${1%.*}".png && rm "$1"
+}
+
+#пустой или нет аргумент
+if [ -z "$1" ]; then
+    echo "Использование:"
+    echo "$0 <путь_к_директории>"
+    echo "$0 <файл>"
+    exit 0
+elif [ -f "$1" ]; then
+	convrt "$1"
+elif [ -d "$1" ]; then
+	cd "$1"
+	for FILE in *.webp
+	do 
+		#dwebp "$FILE" -o "$FILE".png && rm "$FILE"
+		convrt "$FILE"
+	done
+	
+fi
+
