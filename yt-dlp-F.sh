e4d73de9fd81535066c1811e6f42b16eba729f38
B P <aslermelean@gmail.com>
2025-06-05 13:08:01 +0300

    update yt-dlp-F.sh

--- yt-dlp-F.sh
+++ yt-dlp-F.sh
@@ -6,18 +6,13 @@ PATHSAVE="$HOME/.local/bin/yt-dlp"
 #Colours green, blue, yellow, lcyan, normal
 COLOURS=('\033[32m' '\033[01;34m' '\e[1;33m' '\033[1;36m' '\e[0m')
 
-function install ()
-{
-	
+function install (){
+	SOFT="ffmpeg"
 
-SOFT="ffmpeg"
-echo -e ${COLOURS[0]}установка ${SOFT}${COLOURS[4]}
-
-if ! command -v "$SOFT" >/dev/null 2>&1 ; then
-	su -c "apt install \"$SOFT\" -y" -
-		#Мы используем двойные кавычки вокруг всей команды, чтобы оболочка могла интерпретировать переменную $SOFT.
-		#Внутри двойных кавычек мы экранируем переменную $SOFT с помощью обратной косой черты (\), чтобы избежать ее интерпретации до передачи команды в su.
-fi  
+	if ! command -v "$SOFT" >/dev/null 2>&1 ; then
+		echo -e ${COLOURS[0]}установка ${SOFT}${COLOURS[4]}
+		su -c "apt install \"$SOFT\" -y" -
+	fi  
 }
 
 function first () {
@@ -30,8 +25,15 @@ function first () {
 	echo
 	#URL=https://rutube.ru/video/9db722aaa25c825774bfe94da6a4fd04/
 	#LIST=$($PATHSAVE -F $URL | awk '/^-----------/{found=1; next} found')
-	$PATHSAVE -F $URL | awk '/^-----------/{found=1; next} found' > $FILE
-	#echo $LIST
+
+	#HAT=$(yt-dlp -F https://rutube.ru/video/dd2592b46c214006056c3a4fc6cd0f08 | awk '/^ID/')
+	#echo -e "   $HAT"
+
+	#$PATHSAVE -F $URL | awk '/^-----------/{found=1; next} found' > $FILE
+	#HAT=$(yt-dlp -F https://rutube.ru/video/dd2592b46c214006056c3a4fc6cd0f08 | awk '/^ID/')
+	#$PATHSAVE -F $URL | awk 'NR>=7'
+	SELECT=$($PATHSAVE -F $URL | awk 'NR>=7' | fzf --reverse --header-lines=2)
+	#cat $FILE
 }
 
 function second () {
@@ -79,11 +81,10 @@ function four ()
 	$PATHSAVE -f $PART -o "$HOME/Videos/%(title)s.%(ext)s" $URL
 }
 
-function main ()
-{
+function main (){
 	install
 	first
-	second
+	#second
 	tree
 	four
 	rm $FILE
