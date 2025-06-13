90bacd39a8107af152dc91aa652ecf9eecc86586
B P <aslermelean@gmail.com>
2025-06-13 16:35:39 +0300

    add in install_soft.sh func error correct

--- install_soft.sh
+++ install_soft.sh
@@ -5,18 +5,48 @@ function layout_us ()
 	setxkbmap -layout us
 }
 
+# Функция для поиска слов с одной ошибкой
+find_correct_word() {
+    local input_word="$1"
+    local dictionary=("$@")  # Остальные аргументы - это слова из словаря
+    local suggestions=()
+
+    for word in "${dictionary[@]}"; do
+        if [ ${#word} -eq ${#input_word} ]; then
+            # Считаем количество отличий
+            local differences=0
+            for ((i=0; i<${#word}; i++)); do
+                if [ "${word:i:1}" != "${input_word:i:1}" ]; then
+                    ((differences++))
+                fi
+            done
+            if [ $differences -eq 1 ]; then
+                suggestions+=("$word")		# no
+            fi
+        fi
+    done
+
+    # Выводим предложения в fzf
+    if [ ${#suggestions[@]} -gt 0 ]; then
+        SELECT_SOFT=$(echo "${suggestions[@]}" | tr ' ' '\n' | fzf)
+    else
+        echo "Нет предложений."
+    fi
+
+}
+
 echo "Введите название программы для установки"
+layout_us
 read SOFT
 echo "$SOFT" | tr '[:upper:]' '[:lower:]' > /dev/null
-#echo "$SOFT" | tr '[:upper:]' '[:lower:]' > /dev/null
+
+dictionary=($(apt-cache pkgnames))
+find_correct_word "$SOFT" "${dictionary[@]}"
 echo
-apt-cache search "$SOFT"
-#echo
-read a
-echo -e "\napt install \"$SOFT\""
+apt-cache search "$SELECT_SOFT"
 
-layout_us
+echo -e "\napt install $SELECT_SOFT"			#лишн кав были тут
 
-su -c "apt install \"$SOFT\" -y" -
+su -c "apt install \"$SELECT_SOFT\" -y" -
 
 setxkbmap -model pc105 -layout us,ru -option grp:alt_shift_toggle
