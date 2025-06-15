072767a4dae20acbaa99a9514cc72fdfdbb1eecd
B P <aslermelean@gmail.com>
2025-06-15 17:40:24 +0300

    update install_soft

--- install_soft.sh
+++ install_soft.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#если давно обновлялись
+#sudo apt update
+
 function layout_us (){
 	setxkbmap -layout us
 }
@@ -27,7 +30,12 @@ find_correct_word() {
 
     # Выводим предложения в fzf
     if [ ${#suggestions[@]} -gt 0 ]; then
-        SOFT=$(printf "%s\n" "${suggestions[@]}" | fzf --header="Возможно вы имели ввиду:" --reverse --no-info --preview 'apt-cache search {+1} | grep -w {+1}')
+        #SOFT=$(printf "%s\n" "${suggestions[@]}" | fzf --header="Возможно вы имели ввиду:" --reverse --no-info --preview 'apt-cache search {+1} | grep -w {+1}')
+        
+        #SOFT=$(printf "%s\n" "${suggestions[@]}" | fzf --header="Возможно вы имели ввиду:" --reverse --no-info --preview 'apt-cache show {} | grep -w {}')
+        
+        SOFT=$(printf "%s\n" "${suggestions[@]}" | fzf --header="Возможно вы имели ввиду:" --reverse --no-info --preview 'apt-cache show {}')
+        
         #apt-cache search "$SOFT" | grep -w "$SOFT"
         #SELECT_SOFT=$(echo "${suggestions[@]}" | tr ' ' '\n' | fzf)
         #tr ' ' '\n': Команда tr (translate) заменяет символы. В данном случае она заменяет все пробелы (' ') на символы новой строки ('\n').
@@ -37,13 +45,31 @@ find_correct_word() {
 }
 
 func_install(){
-	echo -e "\n$SOFT"
+	#echo -e "\n$SOFT"
 	
 	apt-cache search "$SOFT" | grep -w "$SOFT"
+	#apt-cache search "$SOFT" | grep -w "$SOFT" | fzf --reverse --no-info --preview 'apt-cache show {} | awk "{print $1}"'
+	#apt-cache search "$SOFT" | grep -w "$SOFT" | fzf --reverse --no-info --preview 'apt-cache show {} | awk "{print $1}"'
+	
+	#apt-cache search "$SOFT" | grep -w "$SOFT" | fzf --reverse --no-info --preview '{} | awk "{print \$1}" | apt-cache show -'
+
+
 	#apt search --names-only "$SOFT" | grep -w "$SOFT"
+	#aptitude search "~n$SOFT"
+	#aptitude search "~n$SOFT" | awk '{print $3}'
+	#dictionary=($(apt-cache pkgnames))
 
+    local arrey=()
+    local arrey2=()
+
+		#в массив
+			#второй столбец если описание есть
+			
+		#В другой массим
+			#второй столбец если описание нет
+	echo
+	#apt-cache show $SOFT
 	echo -e "\napt install $SOFT"			#лишн кав были тут
-
 	su -c "apt install \"$SOFT\" -y" -
 
 	setxkbmap -model pc105 -layout us,ru -option grp:alt_shift_toggle
@@ -53,6 +79,7 @@ func_install(){
 echo "Введите название программы для установки"
 layout_us
 read SOFT
+clear
 echo "$SOFT" | tr '[:upper:]' '[:lower:]' > /dev/null
 #SOFT_ORIG="$SOFT"
 
